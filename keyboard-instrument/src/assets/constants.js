4ead77c99706009af2144c2b45ad8cf248b439fd
nmagee_wsbeng <nmagee@wsbeng.com>
2022-11-27 21:32:03 -0600

    restructuring sampler to be more general and in constants

--- keyboard-instrument/src/assets/constants.js
+++ keyboard-instrument/src/assets/constants.js
@@ -1,3 +1,6 @@
+import * as Tone from 'tone'
+
+
 const keyOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     "N0", "N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "N9"];
 
@@ -5,6 +8,31 @@ const types = [
     { name: "Sampler" }, { name: "Frequency" }
 ]
 
-const samplerTypes = ["Casio", "Bongo Kick", "Bongo Snare"];
+const samplerTypes = [{
+    name: "Casio", sampler: new Tone.Sampler({
+        urls: {
+            A1: "A1.mp3",
+            A2: "A2.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/casio/",
+
+    }).toDestination()
+}, {
+    name: "Bongo Kick", sampler: new Tone.Sampler({
+        urls: {
+            A1: "kick.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/drum-samples/Bongos/",
+
+    }).toDestination()
+}, {
+    name: "Bongo Snare", sampler: new Tone.Sampler({
+        urls: {
+            A1: "snare.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/drum-samples/Bongos/",
+
+    }).toDestination()
+}];
 
 export { keyOptions, types, samplerTypes }
--- keyboard-instrument/src/services/casioServices.js
+++ keyboard-instrument/src/services/casioServices.js
@@ -1,38 +1,11 @@
-import * as Tone from 'tone'
-const sampler = new Tone.Sampler({
-    urls: {
-        A1: "A1.mp3",
-        A2: "A2.mp3",
-    },
-    baseUrl: "https://tonejs.github.io/audio/casio/",
 
-}).toDestination();
-
-const synth = new Tone.MembraneSynth().toDestination();
-
-const lowPass = new Tone.Filter({
-    frequency: 8000,
-}).toMaster();
-
-const snareDrum = new Tone.NoiseSynth({
-    volume: 1,
-    noise: {
-        type: 'white',
-        playbackRate: 3,
-    },
-    envelope: {
-        attack: 0.001,
-        decay: 0.20,
-        sustain: 0.15,
-        release: 0.03,
-    },
-}).connect(lowPass).toDestination();
+import { samplerTypes } from "../assets/constants";
 
 
 export default {
 
 
-    callCasioKey(key) {
+    playSamplerKey(key) {
         // let string = key + 1;
         // console.log(string);
         // sampler.triggerAttackRelease([string], 0.5);
